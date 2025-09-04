ed5be0e8333d94bddfc3b410127a3c8eef10afc3
Owen Crokie <oc040324@ohio.edu>
2025-09-04 16:19:03 -0400

    Intial commit

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Owen Crokie
+ *        Date: 09/04/2025
+ * Description: 
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
